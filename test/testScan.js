3f713465b1f7623880cc9dbb7fab50dca37c5b04
IanMeyers <meyersi@amazon.com>
2017-08-11 09:54:19 +0100

    Updated Tests

--- test/testScan.js
+++ test/testScan.js
@@ -4,6 +4,7 @@ var async = require('async');
 var sqn1 = '49550822123942288925422195661801699673398497972964035234';
 var sqn2 = '49550822123942288925422195661802908599218112602138741410';
 var sqn3 = '49550822123942288925422195661802908599218112602138741412';
+var approxArrival = 1428537600;
 
 var everything = function(callback) {
     q.scanArchive('EnergyPipelineSensors', undefined, undefined, undefined, undefined, function(err, item) {
@@ -39,7 +40,24 @@ var by_seq = function(callback) {
     });
 };
 
-async.waterfall([ everything, by_seq ], function(err) {
+var by_approx = function(callback) {
+    q.scanArchive('EnergyPipelineSensors', sqn2, undefined, approxArrival, undefined, function(err, item) {
+	console.log("Scan by Approx Received");
+	if (err) {
+	    console.log(err);
+	} else {
+	    console.log(JSON.stringify(item));
+	}
+    }, function(err) {
+	console.log("Scan by Approx Complete");
+	if (err) {
+	    console.log(err);
+	}
+	callback(err);
+    });
+};
+
+async.waterfall([ everything, by_seq, by_approx ], function(err) {
     console.log("tests complete");
     process.exit(0);
 });
