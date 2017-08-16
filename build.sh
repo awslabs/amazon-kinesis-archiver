c62e26979b018f29e2024f63f06109f613415f55
IanMeyers <meyersi@amazon.com>
2017-08-16 11:12:34 -0700

    Updates for TTL, SAM deployer

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 
 version=`cat package.json | grep version | cut -d: -f2 | sed -e "s/\"//g" | sed -e "s/ //g" | sed -e "s/\,//g"`
 
-functionName=KinesisStreamCompressor
+functionName=amazon-kinesis-archiver
 filename=$functionName-$version.zip
-region=eu-west-1
 
 rm $filename 2>&11 >> /dev/null
 
-zip -r $filename index.js common.js constants.js streamCompressor.js package.json node_modules/ README.md LICENSE.txt && mv -f $filename dist/$filename
-
-aws lambda update-function-code --function-name $functionName --zip-file fileb://dist/$filename --region $region
+zip -r $filename index.js package.json lib/ node_modules/ README.md LICENSE.txt && mv -f $filename dist/$filename
