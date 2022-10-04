648b2bbe3fe4851b714fd99ed939ca4f66a812dd
Ian Meyers <meyersi@amazon.com>
2022-10-04 13:45:22 +0100

    Version 1.0.2
    Updating async dependency.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #set -x
 
-ver=1.0.1
+ver=1.0.2
 
 for r in `aws ec2 describe-regions --query Regions[*].RegionName --output text`; do aws s3 cp dist/amazon-kinesis-archiver-$ver.zip s3://awslabs-code-$r/AmazonKinesisArchiver/amazon-kinesis-archiver-$ver.zip --acl public-read --region $r; done
